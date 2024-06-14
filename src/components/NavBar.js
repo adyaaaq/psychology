3bb5a81d2e89dbee708a8e6535c7790b1a5c10b4
Bat-Ireedui <d.adiyabazar006@gamil.com>
2024-06-14 15:40:06 +0800

    fixed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaBars, FaArrowLeft } from "react-icons/fa";
 
 const NavBar = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
+    document.querySelector(".menu-icon").classList.toggle("rotate");
+  };
+
   return (
     <div className="nav">
       <div className="logo">
@@ -13,7 +21,29 @@ const NavBar = () => {
         <a href="/news">Насны онцлог</a>
         <a href="/quiz">Тест</a>
         <a href="/books">Шинэ номнууд</a>
-        <a href="/">Туршилтууд</a>
+        <a href="/">Сэтгэл судлалын онолууд</a>
+      </div>
+      <div className="menu-icon" onClick={toggleModal}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <div
+        className={`modal ${showModal ? "show" : "hide"}`}
+        onClick={toggleModal}
+      >
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <div className="back-button" onClick={toggleModal}>
+            <FaArrowLeft size={20} style={{ fontWeight: "lighter" }} /> Буцах
+          </div>
+          <div className="modal-links">
+            <a href="/">Нүүр хуудас</a>
+            <a href="/news">Насны онцлог</a>
+            <a href="/quiz">Тест</a>
+            <a href="/books">Шинэ номнууд</a>
+            <a href="/">Сэтгэл судлалын онолууд</a>
+          </div>
+        </div>
       </div>
     </div>
   );
