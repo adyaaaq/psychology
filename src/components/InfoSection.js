d692ac6357a4f6d81c20ebd6d0c546021809c6b3
Bat-Ireedui <d.adiyabazar006@gamil.com>
2024-06-14 16:26:20 +0800

    hey fixed

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,23 +1,23 @@
 import React from "react";
-import Card from "../components/NewsCard"
-import newsItems from "../assets/json"; 
-import booksData from "../assets/books"; 
+import Card from "../components/NewsCard";
+import newsItems from "../assets/json";
+import booksData from "../assets/books";
 import BookCard from "./BookCard";
 import ageCategories from "../assets/ages";
 import AgeCard from "../components/ageCard";
 const InfoSection = () => {
   return (
     <div className="infoSection">
-      <div className="infoHeader" data-aos="fade-up" data-aos-duration="1000">
-        <h1>
-          Things you could do during the <br />{" "}
-          <span style={{ color: "#e0501b" }}>Coronavirus Quarantine.</span>{" "}
-        </h1>
-      </div>
+      <div
+        className="infoHeader"
+        data-aos="fade-up"
+        data-aos-duration="1000"
+      ></div>
       <div className="infoCards">
-      {ageCategories.map((ageCategory) => (
-        <AgeCard key={ageCategory.id} ageCard={ageCategory} />))}
-    </div>
+        {ageCategories.map((ageCategory) => (
+          <AgeCard key={ageCategory.id} ageCard={ageCategory} />
+        ))}
+      </div>
 
       <div className="infoCards">
         {booksData.map((book) => (
