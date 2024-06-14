c30da6af9fd19103c6019605983004184deefa8f
Bat-Ireedui <d.adiyabazar006@gamil.com>
2024-06-14 14:44:33 +0800

    yanzaga

--- src/pages/Books.js
+++ src/pages/Books.js
@@ -6,12 +6,10 @@ import Navbar from "../components/NavBar";
 export default function Books() {
   return (
     <div>
-     
       {/* <h1>
       Шинэхэн номнууд
     </h1> */}
       <div className="new-books">
-         <Navbar />
         {booksData.map((book) => (
           <BookCard key={book.id} book={book} />
         ))}
