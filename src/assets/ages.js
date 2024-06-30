b6a33eeca803f599ad7fe1a99d94090ab2a6494e
adiya <d.adiyabazar006@gmail.com>
2024-06-30 18:19:04 +0800

    images inserted
    asd

--- src/assets/ages.js
+++ src/assets/ages.js
@@ -1,66 +1,80 @@
 // newsData.js
-import emeImage from "../components/img/generations/eme.png";
+import a from "../components/img/generations/eme.png";
+import a2 from "../components/img/generations/2.png";
+import a3 from "../components/img/generations/3.png";
+import a4 from "../components/img/generations/4.png";
+import a5 from "../components/img/generations/5.png";
+import a6 from "../components/img/generations/6.png";
+import a7 from "../components/img/generations/7.png";
+import a8 from "../components/img/generations/8.png";
+import a9 from "../components/img/generations/9.png";
+import a10 from "../components/img/generations/10.png";
+import a11 from "../components/img/generations/11.png";
+import a12 from "../components/img/generations/12.png";
+import a13 from "../components/img/generations/13.png";
+import a14 from "../components/img/generations/14.png";
+import a15 from "../components/img/generations/16.png";
 
 const ageCategories = [
   {
     id: 1,
     title: "22-25",
     details: " Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi. Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nis  Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi. ##  Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi. Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nis  Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi. ##  Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi. Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nis  Velit minim amet mollit ad ipsum dolore officia occaecat. Nostrud consequat est cupidatat veniam commodo irure dolor non. Sit qui in sit sunt ad cupidatat sit aliquip id labore voluptate consectetur velit occaecat. Dolore aliquip occaecat esse dolor ut consequat mollit irure consequat sunt nisi.",
-    imageUrl: emeImage,
+    imageUrl: a,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a2,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a3,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a4,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a5,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a6,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a7,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a8,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a9,
   },
   {
     id: 2,
     title: "32-42",
     details: "Details 2",
-    imageUrl: emeImage,
+    imageUrl: a10,
   },
   // Add more news items as needed
 ];
